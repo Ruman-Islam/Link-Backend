37b2c54a9a40925948c4f280ec05ad198fe54fa6
Ruman-Islam <rumanislam48@gmail.com>
2022-12-08 02:53:30 +0600

    bug fixing

--- utils/app.js
+++ utils/app.js
@@ -9,7 +9,8 @@ const adminRoute = require("../routes/v1/admin.route");
 // APPLICATION MIDDLEWARE //
 app.use(cors());
 app.use(express.json());
-app.use(express.static("./public"));
+// app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "../public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views/pages"));
 
