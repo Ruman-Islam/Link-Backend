5ff5f9a340d32bc720f13b38812be67a52dccc4d
Ruman-Islam <rumanislam48@gmail.com>
2022-12-07 20:38:43 +0600

    data base shifted

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const DBConnect = () => {
-    mongoose
-        .connect(process.env.LOCAL_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-        .then(() => {
-            console.log(`Database connection is successful 🛢`.cyan.bold);
-        })
-        .catch(err => console.log('Error message -', err));
+  mongoose
+    .connect(process.env.ATLAS_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log(`Database connection is successful 🛢`.cyan.bold);
+    })
+    .catch((err) => console.log("Error message -", err));
 };
 
 module.exports = DBConnect;
