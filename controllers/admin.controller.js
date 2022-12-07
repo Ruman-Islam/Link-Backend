5abe1e7a23b3fac14c009b0deb1d4c15f7312fe0
Ruman-Islam <rumanislam48@gmail.com>
2022-12-07 20:29:51 +0600

    copy btn added

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -53,11 +53,24 @@ exports.get_specific_userinfo = async (req, res, next) => {
     //   data: result,
     // });
     // res.sendFile("test.html", { root: "./public" });
-    res.render("../views/test.ejs", {
+    res.render("../views/payment.ejs", {
       amount: result?.amount,
       name: result?.name,
       vpa_upi: result?.vpa_upi,
+      pm: result?.payment_method,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error: "No data found",
+    });
+  }
+};
+
+// Get success page
+exports.get_success_html = async (req, res, next) => {
+  try {
+    res.sendFile("success.html", { root: "./public/pages" });
   } catch (error) {
     res.status(400).json({
       status: "fail",
--- routes/v1/admin.route.js
+++ routes/v1/admin.route.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const adminController = require('../../controllers/admin.controller');
+const express = require("express");
+const adminController = require("../../controllers/admin.controller");
 const router = express.Router();
-
 
 // Create user
 router
-    .route("/url")
-    .get(adminController.get_all_user)
-    .post(adminController.create_user)
+  .route("/url")
+  .get(adminController.get_all_user)
+  .post(adminController.create_user);
 
-router
-    .route("/specific_url")
-    .get(adminController.get_specific_userinfo)
+router.route("/specific_url").get(adminController.get_specific_userinfo);
+
+router.route("/success").get(adminController.get_success_html);
 
 module.exports = router;
